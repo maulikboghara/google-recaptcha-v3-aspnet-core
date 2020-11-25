dd79cc3ceb8a0460c560f7b6d781d12c3f0628d8
Maulik Boghara <maulikboghara@gmail.com>
2020-11-25 21:36:17 +0530

    Add project files.

---
+++ GoogleCaptchaV3CoreDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoogleCaptchaV3CoreDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
